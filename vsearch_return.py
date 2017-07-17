bd63853db56509a834c280c08f1392f647847438
pkgthesh <pkgthesh@gmail.com>
2017-07-17 20:13:36 +0100

    Adding more function programs

---
+++ vsearch_return.py
@@ -0,0 +1,5 @@
+def search4vowels(word):
+        """ Return a boolean based on any vowels found"""
+        vowels = set ('aeiou')
+        found = vowels.intersection(set(word))
+        return bool(found)
---
+++ vsearch_return_multiple.py
@@ -0,0 +1,4 @@
+def search4vowels(word):
+        """ Return a boolean based on any vowels found"""
+        vowels = set ('aeiou')
+        return vowels.intersection(set(word))
