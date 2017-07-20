ba9812a8970bdba6f19da3984a2daaa2e5895a83
pkgthesh <pkgthesh@gmail.com>
2017-07-20 06:07:23 +0100

    Adding Modules and WebApps

---
+++ mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='Python Search Tools',
+    author='jith',
+    author_email='jith@test.com',
+    url='testdomain.com',
+    py_modules=['vsearch'],
+)
